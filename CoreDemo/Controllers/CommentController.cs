c22906939bc915e6df6c7d4401058419e80b2884
temrecelik <temrecelik21@gmail.com>
2024-05-14 01:23:16 +0300

    The project has been developed

--- CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 
@@ -14,9 +15,25 @@ namespace CoreDemo.Controllers
 		 Controllerde parçalı Iaction result fonksiyonu PartailViewResult ile yazılır.
 		 */
 		CommentManager cm = new CommentManager (new EfCommentRepository());
+		public IActionResult ındex()
+		{
+			return View();	
+		}
 
+
+		[HttpGet]
 		public PartialViewResult PartialAddComment()
 		{
+			return PartialView();
+		}
+		[HttpPost]
+		public PartialViewResult PartialAddComment(Comment p)
+		{
+			p.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+			p.CommentStatus = true;
+			p.BlogID = 30;
+			cm.CommentAdd(p);
+
 			return PartialView();
 		}
 
