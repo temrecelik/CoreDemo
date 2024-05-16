e2441e054af7e0cae9e9d869887367a56679f9fa
temrecelik <temrecelik21@gmail.com>
2024-05-16 15:12:57 +0300

    Project has been developed

---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+/*
+ Authorization İşlemleri
+-Yetkilendirme işlemleridir.
+-Bir kişinin sisteme giriş yapmadan erişemeyeceği sayfalar ve özelliklerdir.
+[Authorize] şeklinde attribute ile authorizing işlmeleri yapılır
+
+Bir yazarın websitesinde olacak nitelikleri
+	-Profili
+	-Mesaj
+	-Blogları
+	-Giriş Şifreleri
+	-Ayarlar 
+	
+ */
+
+
+namespace CoreDemo.Controllers
+{ 
+	//[Authorize] buraya yazarsak tüm Actionlara girmek için Authorize gerçekleştirmesi lazım
+	public class WriterController : Controller
+	{
+		/*Otantike olmadığımız sürece /Writer/Index/ sayfasına giremeyiz */
+		[Authorize]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult WriterProfile()
+		{
+			return View();
+		}
+
+		public IActionResult WriterMail(){ 
+			return View();
+		}
+
+	}
+}
