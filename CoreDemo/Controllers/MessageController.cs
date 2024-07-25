3dc3660f42c7776098afd40536ea11f73c2f0a66
temrecelik <temrecelik21@gmail.com>
2024-07-25 21:04:29 +0300

    Message Update!

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+
+        Message2Manager mn = new Message2Manager(new EfMessage2Repository());
+
+        public IActionResult InBox()
+        {
+            int id = 2;
+            var values = mn.GetInboxListByWriter(id);
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult MessageDetails(int id)
+        {
+
+            var value = mn.TGetById(id);
+            return View(value);
+        }
+    }
+}
